31a1976affbfb2d09ee6772fddc18e66e45619f5
BalaJAT <142164559+BalaJAT@users.noreply.github.com>
2023-09-21 15:53:15 +0530

    Add files via upload

---
+++ SeniorCitizenChecker.java
@@ -0,0 +1,22 @@
+package guviJavaprograms;
+
+import java.util.Scanner;
+
+public class SeniorCitizenChecker {
+	
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter your age: ");
+	        int age = scanner.nextInt();
+
+	        if (age >= 60) {
+	            System.out.println("You are a senior citizen.");
+	        } else {
+	            System.out.println("You are not a senior citizen.");
+	        }
+
+	        scanner.close();
+	    }
+
+}
