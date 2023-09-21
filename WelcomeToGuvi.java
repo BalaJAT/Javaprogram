97d6c55648830621a47c931065a455921eb0376f
BalaJAT <142164559+BalaJAT@users.noreply.github.com>
2023-09-21 15:48:00 +0530

    Add files via upload

---
+++ WelcomeToGuvi.java
@@ -0,0 +1,9 @@
+package guviJavaprograms;
+
+public class WelcomeToGuvi {
+	   public static void main(String[] args) {
+	        for (int i = 0; i < 10; i++) {
+	            System.out.println("Welcome to Guvi");
+	        }
+	    }
+}
