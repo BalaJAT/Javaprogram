35eac876a8b0cef7164ee82e76b8ef43ca334018
BalaJAT <142164559+BalaJAT@users.noreply.github.com>
2023-09-21 14:33:53 +0530

    Update Sumcomparison.java

--- Sumcomparison.java
+++ Sumcomparison.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 
 public class Sumcomparison {
 	public static void main(String[] args) {
-        // Create a Scanner object for user input
+
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter values for a, b, c, and d
         System.out.print("Enter the value of 'a': ");
         int a = scanner.nextInt();
 
@@ -20,10 +19,8 @@ public class Sumcomparison {
         System.out.print("Enter the value of 'd': ");
         int d = scanner.nextInt();
 
-        // Close the scanner to prevent resource leak
         scanner.close();
 
-        // Check whether the sum of a and b is greater than the sum of c and d
         if (a + b > c + d) {
             System.out.println("The sum of a and b is greater than the sum of c and d.");
         } else {
