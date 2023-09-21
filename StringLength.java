698cc4975dc873b787598d455faf8cedee55cdd1
BalaJAT <142164559+BalaJAT@users.noreply.github.com>
2023-09-21 15:44:20 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,14 @@
+package guviJavaprograms;
+
+public class StringLength {
+	
+	public static void main(String[] args) {
+   
+        String inputString = "GuviGeek";
+
+        int length = inputString.length();
+
+        System.out.println("Length of the string is: " + length);
+    }
+
+}
